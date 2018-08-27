2478cb28ff1f1c92f19b01586c83a3bb30f9775a
gsxxx <aide@aide.egeriastaz.krakow.comarch>
2018-08-27 15:22:19 +0200

    adding -car options to backup.sh, small fixes in installClient

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,9 +2,9 @@
 HOME_DIR="/home/aide/aide"
 TEST_MODE=0
 INIT_MODE=0
-FILES_CHANGE=()
-FILES_ADD=()
-FILES_REMOVE=()
+FILES_TO_CHANGE=()
+FILES_TO_ADD=()
+FILES_TO_REMOVE=()
 usage () {
 	echo -e "Usage: backup.sh -h hostname <-i | <[-c file...] [-a file...] [-r file...]> [-t]"
 	exit 1
@@ -16,6 +16,7 @@ error () {
 	echo -e "[\e[31mERROR\e[0m] $1"
 	exit ${2}
 }
+if [[ $# -lt 3 ]] ;then usage ;fi
 while [[ $# -gt 0 ]]; do
 i=$1
 	case $i in
@@ -32,18 +33,18 @@ i=$1
 			shift
 		;;
 		-a)
-			FILES_ADD+=($2)
-			LAST_ARRAY="FILES_ADD"
+			FILES_TO_ADD+=($2)
+			LAST_ARRAY="FILES_TO_ADD"
 			shift 2
 		;;
 		-r)
-			FILES_REMOVE+=($2)
-			LAST_ARRAY="FILES_REMOVE"
+			FILES_TO_REMOVE+=($2)
+			LAST_ARRAY="FILES_TO_REMOVE"
 			shift 2
 		;;
 		-c)
-			FILES_CHANGE+=($2)
-			LAST_ARRAY="FILES_CHANGE"
+			FILES_TO_CHANGE+=($2)
+			LAST_ARRAY="FILES_TO_CHANGE"
 			shift 2
 		;;
 		*)	if [ ! -z ${LAST_ARRAY} ]; then
@@ -55,27 +56,21 @@ i=$1
 		;;
 	esac
 done
-if [[ ${TEST_MODE} ]];then
-	echo "FILES_CHANGE: ${FILES_CHANGE[@]}"
-	echo "FILES_ADD: ${FILES_ADD[@]}"
-	echo "FILES_REMOVE: ${FILES_REMOVE[@]}"
-fi
-if [ -z ${SERVER} ]; then
+if [ -z ${SERVER} ] || [[ ${INIT_MODE} == 1 ]] && ( [ ${#FILES_TO_CHANGE[@]} -ne 0 ] || [ ${#FILES_TO_ADD[@]} -ne 0 ] || [ ${#FILES_TO_REMOVE[@]} -ne 0 ] ); then
 	usage
 fi
-#clear
 if [ ! -d ${HOME_DIR}/clients/${SERVER}/backup ]; then
-	error "No such client or his structure of file corrupted." 2
+	error "No such client or his structure of file is corrupted." 2
 else
 	ok "Client has been found."
 fi
-TAR_COMMAND="ssh aide_spool@${SERVER} sudo tar cvzf -"
 if [[ ${INIT_MODE} == 1 ]]; then
 	if [ -f ${HOME_DIR}/conf/${SERVER}.conf ]; then
 		ok "Client's file has been found."
 	else
 		error "Client's config has not been found." 3
 	fi
+	TAR_COMMAND="ssh aide_spool@${SERVER} sudo tar cvf -"
 	while IFS='' read -r input || [[ -n "${input}" ]]; do
 		if echo ${input} | grep -P "^/.*CONTENT_EX"> /dev/null; then
 			TAR_COMMAND=${TAR_COMMAND}" "$(echo ${input} | sed -r 's|^([^~\*$\\ ]+).*|\1|')
@@ -83,12 +78,48 @@ if [[ ${INIT_MODE} == 1 ]]; then
 			TAR_COMMAND=${TAR_COMMAND}" --exclude='"$(echo ${input} | sed -r 's|^\!||' | sed -r 's|^([^~\*$\.\\ ]+).*|\1|')"'"
 		fi
 	done < ${HOME_DIR}/conf/${SERVER}.conf
-	TAR_COMMAND=${TAR_COMMAND}" > ${HOME_DIR}/clients/${SERVER}/backup/backup-$(date +%s).tar.gz"
+	TAR_COMMAND=${TAR_COMMAND}" > ${HOME_DIR}/clients/${SERVER}/backup/dump-$(date +%s).tar.gz"
 	if [[ ${TEST_MODE} == 1 ]]; then
 		echo ${TAR_COMMAND}
 	else
-		eval ${TAR_COMMAND}
+		if eval ${TAR_COMMAND} 2>/dev/null; then
+			ok "New dump initialized."
+		else
+			error "Something went wrong during dump initialization." 4
+		fi
 	fi
 else
-	echo "this part of script is under constraction"
+	echo "this part of script is under construction"
+	if [[ ${TEST_MODE} == 1 ]];then
+		echo "FILES TO CHANGE: ${FILES_TO_CHANGE[@]}"
+		echo "FILES TO ADD: ${FILES_TO_ADD[@]}"
+		echo "FILES TO REMOVE: ${FILES_TO_REMOVE[@]}"
+	fi
+	if $(ls ${HOME_DIR}/clients/${SERVER}/backup | grep -P "dump-[0-9]{10}.tar.gz" > /dev/null); then
+		BACKUP_FILE=$(ls ${HOME_DIR}/clients/${SERVER}/backup | grep -P "dump-[0-9]{10}.tar.gz" | sort -r | sed -ne '1p')
+		BACKUP_DATE=$(echo ${BACKUP_FILE} | grep -Po "[0-9]{10}")
+		ok "Dump from $(date -d@${BACKUP_DATE} +%D-%T) has been found."
+	else
+		error "Client does not have dump initialized." 5
+	fi
+	BACKUP_FILE_PATH="${HOME_DIR}/clients/${SERVER}/backup/${BACKUP_FILE}"
+	TAR_COMMAND="tar zxf ${BACKUP_FILE_PATH} --xform='s#^.+/##x' --xform='s#.*#&.$(date +%s).old#x' -C ${HOME_DIR}/clients/${SERVER}/recovery/"
+	for file in ${FILES_TO_CHANGE[@]}; do 
+		TAR_COMMAND+=" "$(echo ${file} | sed 's#^/##')
+ 	done
+	if eval ${TAR_COMMAND} 2>/dev/null 1>/dev/null;then 
+		ok "New files in recovery directory"
+	else
+		error "Something went wrong with extracting files from archive." 6
+	fi
+	if_init=0
+	for file in ${FILES_TO_ADD[@]}; do 
+		if_init=1
+	done
+	for file in ${FILES_TO_REMOVE[@]}; do 
+		if_init=1
+	done
+	if [[ ${if_init} == 1 ]]; then
+		${HOME_DIR}/scripts/bash.sh -h ${SERVER} -i
+	fi
 fi
